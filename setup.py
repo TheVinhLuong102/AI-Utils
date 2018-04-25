1f38b64e84311521c614aba4879aa99b4097e962
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-04-25 01:06:37 -0700

    add skeletal packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='Arimo-SDK-dev',
+    author='Arimo LLC (a Panasonic company)',
+    author_email='info@arimo.com',
+    url='https://github.com/adatao/SDK-dev',
+    version='0.0.0',
+    description='Arimo SDK dev',
+    long_description='Arimo SDK dev',
+    keywords='Arimo SDK dev',
+    packages=find_packages())
