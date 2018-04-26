bc86d77bdfdb06d329509806f36805ebfacb2874
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-04-26 08:56:03 -0700

    minor edits to arimo.blueprints

--- arimo/blueprints/__init__.py
+++ arimo/blueprints/__init__.py
@@ -1,7 +1,8 @@
 try:
-  import pkg_resources
-  pkg_resources.declare_namespace(__name__)
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
 
 except ImportError:
-  import pkgutil
-  pkgutil.extend_path(__path__, __name__)
+    import pkgutil
+    pkgutil.extend_path(__path__, __name__)
+
