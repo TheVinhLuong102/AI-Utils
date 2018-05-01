b4575132a8a249f94ffbf9566c726899479f3571
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-01 00:36:29 -0700

    optimize nthreads in arimo.df.spark_from_files.FileADF

--- arimo/df/spark_from_files.py
+++ arimo/df/spark_from_files.py
@@ -1179,7 +1179,7 @@ class FileADF(_FileDFABC, ADF):
         pandasDF = \
             self._pieceArrowTable(pieceSubPath) \
                 .to_pandas(
-                    nthreads=psutil.cpu_count(logical=True) - 2,
+                    nthreads=max(1, psutil.cpu_count(logical=True) // 2),
                     strings_to_categorical=False,
                     memory_pool=None,
                     zero_copy_only=True)
