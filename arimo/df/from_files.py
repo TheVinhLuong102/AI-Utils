4c2bba56d5a2f9052417b3157c78d8bbb319a624
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-02 15:24:49 -0700

    fix minor typo in arimo.df.from_files

--- arimo/df/from_files.py
+++ arimo/df/from_files.py
@@ -41,7 +41,7 @@ from arimo.util import DefaultDict, fs, Namespace
 from arimo.util.aws import s3
 from arimo.util.date_time import gen_aux_cols, DATE_COL
 from arimo.util.decor import enable_inplace, _docstr_settable_property, _docstr_verbose
-from arimo.util.srcTypes.arrow import \
+from arimo.util.types.arrow import \
     _ARROW_INT_TYPE, _ARROW_DOUBLE_TYPE, _ARROW_STR_TYPE, _ARROW_DATE_TYPE, \
     is_boolean, is_complex, is_num, is_possible_cat, is_string
 import arimo.debug
