0604fa6ed440f767c07752c770050f3fe750c38f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-05-21 17:25:44 -0700

    update test script

--- test/test-FileADF-prep-data.py
+++ test/test-FileADF-prep-data.py
@@ -1,9 +1,10 @@
 import numpy
 
-from arimo.df.spark_on_files import FileADF
+from arimo.df.spark_from_files import ArrowSparkADF
+from arimo.IoT.PredMaint import project
 
-from PanasonicColdChain import _AWS_ACCESS_KEY_ID, _AWS_SECRET_ACCESS_KEY
-from PanasonicColdChain.PredMaint.PPPAD import PRED_MAINT_PROJECT
+
+PROJECT = project('PanaCC')
 
 
 EQUIPMENT_GENERAL_TYPE = 'refrig'
@@ -11,34 +12,32 @@ EQUIPMENT_UNIQUE_TYPE = 'inverter_2_multi_comp_refrigerator'
 
 EQUIPMENT_INSTANCE_ID = '1018_c34'
 
-BLUEPRINT_UUID = '7a57137a-ef01-4c77-9362-54aa2a55bcf2'
+BLUEPRINT_UUID = 'REFRIG---inverter_2_multi_comp_refrigerator---to-2015-12---20e7dfd0-dcfb-4ec0-b597-def65ecca5c5'
 
 LABEL_VARS = 'low_pressure', 'high_pressure', 'suction_temperature'
 
 DATE = '2016-07-01'
 
 
-bp = PRED_MAINT_PROJECT._ppp_blueprint(
+bp = PROJECT._ppp_blueprint(
     uuid=BLUEPRINT_UUID,
     verbose=False)
 
 
-fadf0 = PRED_MAINT_PROJECT.load_equipment_data(EQUIPMENT_INSTANCE_ID, iCol=None)
+fadf0 = PROJECT.load_equipment_data(EQUIPMENT_INSTANCE_ID, iCol=None, _spark=True)
 
 fadf1 = fadf0.filterByPartitionKeys(('date', DATE))
 
 if '__disp_cases__' in fadf1.columns:
     path = fadf1.path + '---FIXED'
-
     fadf1.drop('__disp_cases__') \
         .save(path=path,
-              aws_access_key_id=_AWS_ACCESS_KEY_ID,
-              aws_secret_access_key=_AWS_SECRET_ACCESS_KEY)
-
-    fadf1 = FileADF(
+              aws_access_key_id=PROJECT.params.s3.access_key_id,
+              aws_secret_access_key=PROJECT.params.s3.secret_access_key)
+    fadf1 = ArrowSparkADF(
         path=path,
-        aws_access_key_id=_AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=_AWS_SECRET_ACCESS_KEY,
+        aws_access_key_id=PROJECT.params.s3.access_key_id,
+        aws_secret_access_key=PROJECT.params.s3.secret_access_key,
         tCol='date_time')
 
 
@@ -65,16 +64,16 @@ for label_var in LABEL_VARS:
             numpy.vstack(
                 v.toArray()
                 for v in score_prep_df1_from_spark['__Xvec__' + label_var])
-    
     component_bp_data_params = bp.params.model.component_blueprints[label_var].data
-
     Xs_from_pandas[label_var] = \
         X_from_pandas = \
             score_prep_df1[
                 list(component_bp_data_params._cat_prep_cols +
                      component_bp_data_params._num_prep_cols)].values
-
     assert numpy.allclose(
         X_from_spark,
         X_from_pandas,
         atol=1e-4)
+
+
+df = fadf1.toPandas()
