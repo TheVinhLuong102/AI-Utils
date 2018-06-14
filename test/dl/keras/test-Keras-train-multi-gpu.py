4846cd2ad1ff78a77ef1a0b659ad14c4abeb0360
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-14 15:49:13 -0700

    update test/dl/keras/test-Keras-train-multi-gpu script

--- test/dl/keras/test-Keras-train-multi-gpu.py
+++ test/dl/keras/test-Keras-train-multi-gpu.py
@@ -8,126 +8,42 @@ import arimo.backend
 x = y = numpy.array([[-3], [-2], [-1], [1], [2], [3]])
 
 
-
-# *** BAD!!! ***
-model___merge___reloc = \
-  arimo.backend.keras.models.Sequential(
-    layers=[arimo.backend.keras.layers.Dense(
-      input_shape=(1,),
-      units=1,
-      activation='linear',
-      use_bias=True)])
-
-y0 = model___merge___reloc.predict(x)
-
-multi_gpu_model___merge___reloc = \
-    arimo.backend.keras.utils.multi_gpu_model(
-        model___merge___reloc,
-        gpus=2,
-        cpu_merge=True,
-        cpu_relocation=True)
-
-multi_gpu_model___merge___reloc.compile(
-    loss='mae',
-    optimizer=arimo.backend.keras.optimizers.Nadam())
-
-multi_gpu_model___merge___reloc.fit(
-    x=x, y=y,
-    batch_size=1,
-    epochs=100,
-    verbose=2)
-
-y1 = model___merge___reloc.predict(x)
-
-print(numpy.hstack((y0 - y, y1 - y)))
-
-
-# *** BAD ***
-model___merge___no_reloc = \
-    arimo.backend.keras.models.Sequential(
+def train(merge=False, reloc=False):
+    model = arimo.backend.keras.models.Sequential(
         layers=[arimo.backend.keras.layers.Dense(
-            input_shape=(1,),
-            units=1,
-            activation='linear',
-            use_bias=True)])
+                    input_shape=(1,),
+                    units=1,
+                    activation='linear',
+                    use_bias=True,
+                    kernel_initializer='zeros',
+                    bias_initializer='ones')])
 
-y0 = model___merge___no_reloc.predict(x)
+    y0 = model.predict(x)
 
-multi_gpu_model___merge___no_reloc = \
-    arimo.backend.keras.utils.multi_gpu_model(
-        model___merge___no_reloc,
-        gpus=2,
-        cpu_merge=True,
-        cpu_relocation=False)
+    multi_gpu_model = \
+        arimo.backend.keras.utils.multi_gpu_model(
+            model,
+            gpus=2,
+            cpu_merge=merge,
+            cpu_relocation=reloc)
 
-multi_gpu_model___merge___no_reloc.compile(
-    loss='mae',
-    optimizer=arimo.backend.keras.optimizers.Nadam())
+    multi_gpu_model.compile(
+        loss='mae',
+        optimizer=arimo.backend.keras.optimizers.Nadam())
 
-multi_gpu_model___merge___no_reloc.fit(
-    x=x, y=y,
-    batch_size=1,
-    epochs=9,
-    verbose=1)
+    hist = multi_gpu_model.fit(
+        x=x, y=y,
+        batch_size=1,
+        epochs=100,
+        verbose=0)
 
-y1 = model___merge___no_reloc.predict(x)
+    y1 = model.predict(x)
 
-print(numpy.hstack((y0 - y, y1 - y)))
-
-
-# *** BAD!!! ***
-model___no_merge___reloc = \
-    arimo.backend.keras.models.Sequential(
-        layers=[arimo.backend.keras.layers.Dense(
-            input_shape=(1,),
-            units=1,
-            activation='linear',
-            use_bias=True)])
-
-multi_gpu_model___no_merge___reloc = \
-    arimo.backend.keras.utils.multi_gpu_model(
-        model___no_merge___reloc,
-        gpus=2,
-        cpu_merge=True,
-        cpu_relocation=False)
-
-multi_gpu_model___no_merge___reloc.compile(
-    loss='mae',
-    optimizer=arimo.backend.keras.optimizers.Nadam())
-
-multi_gpu_model___no_merge___reloc.fit(
-    x=x, y=y,
-    batch_size=1,
-    epochs=9,
-    verbose=1)
-
-print(model___no_merge___reloc.predict(x))
+    print(hist.history['loss'][-1])
+    return numpy.hstack((y0, y1)) - y
 
 
-# *** BAD!!! ***
-model___no_merge___no_reloc = \
-    arimo.backend.keras.models.Sequential(
-        layers=[arimo.backend.keras.layers.Dense(
-            input_shape=(1,),
-            units=1,
-            activation='linear',
-            use_bias=True)])
-
-multi_gpu_model___no_merge___no_reloc = \
-    arimo.backend.keras.utils.multi_gpu_model(
-        model___no_merge___no_reloc,
-        gpus=2,
-        cpu_merge=True,
-        cpu_relocation=False)
-
-multi_gpu_model___no_merge___no_reloc.compile(
-    loss='mae',
-    optimizer=arimo.backend.keras.optimizers.Nadam())
-
-multi_gpu_model___no_merge___no_reloc.fit(
-    x=x, y=y,
-    batch_size=1,
-    epochs=9,
-    verbose=1)
-
-print(model___no_merge___no_reloc.predict(x))
+print(train(merge=False, reloc=False))
+print(train(merge=True, reloc=False))
+print(train(merge=False, reloc=True))
+print(train(merge=True, reloc=True))
