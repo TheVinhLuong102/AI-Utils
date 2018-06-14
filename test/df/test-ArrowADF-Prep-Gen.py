2578907bcbd171c35176b86a81c7e7ce325619fa
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-13 17:25:59 -0700

    update test-ArrowADF-Prep-Gen script

--- test/df/test-ArrowADF-Prep-Gen.py
+++ test/df/test-ArrowADF-Prep-Gen.py
@@ -16,7 +16,7 @@ else:
     EQ_UNQ_TP_GRP = 'inverter_4_multi_comp_refrigerator'
     LABEL_VAR = 'suction_temperature'
 
-SEQ_LEN = 9
+SEQ_LEN = 1   # 9
 
 BATCH_SIZE = 10 ** 3
 
@@ -35,16 +35,20 @@ ppp_bp = PROJECT._ppp_blueprint(
 arrow_adf = PROJECT.load_equipment_data(
     '{}---{}'.format(EQ_GEN_TP.upper(), EQ_UNQ_TP_GRP),
     _on_files=True, _spark=False,
-    iCol=SEQ_LEN > 1)
+    set_i_col=SEQ_LEN > 1)
 
 
 prep_arrow_adf = ppp_bp.prep_data(df=arrow_adf, __mode__='train')[LABEL_VAR]
 
 component_bp_params = ppp_bp.params.model.component_blueprints[LABEL_VAR]
 
+feature_cols = component_bp_params.data._cat_prep_cols + component_bp_params.data._num_prep_cols
+
+
+# *** ArrowADF.gen(...) ***
 gen_instance = \
     prep_arrow_adf.gen(
-        component_bp_params.data._cat_prep_cols + component_bp_params.data._num_prep_cols +
+        feature_cols +
         ((- component_bp_params.pred_horizon_len - component_bp_params.max_input_ser_len + 1,
           - component_bp_params.pred_horizon_len)
          if SEQ_LEN > 1
@@ -57,13 +61,32 @@ gen_instance = \
         anon=True,
         collect='numpy',
         pad=numpy.nan,
-        filter={},
-        n_threads=N_THREADS)
+        filter={component_bp_params.data.label.var: (-100, 100)},
+        nThreads=N_THREADS)
 
+print(gen_instance)
 print(gen_instance.colsLists)
 
 
-g = gen_instance()
+g0 = gen_instance()
 
 for _ in tqdm.tqdm(range(N_BATCHES)):
-    x, y = next(g)
+    x, y = next(g0)
+
+
+# *** S3ParquetDataset(Queue)Reader.generate_chunk(...) ***
+cross_sect_dldf = \
+    prep_arrow_adf._CrossSectDLDF(
+        feature_cols=feature_cols,
+        target_col=component_bp_params.data.label.var,
+        n=BATCH_SIZE,
+        filter={component_bp_params.data.label.var: (-100, 100)},
+        nThreads=N_THREADS)
+
+print(cross_sect_dldf)
+
+
+g1 = cross_sect_dldf.generate_chunk()
+
+for _ in tqdm.tqdm(range(N_BATCHES * BATCH_SIZE / (10 ** 4))):
+    chunk_df = next(g1)
