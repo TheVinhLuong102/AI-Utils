04f1b6cc2bcfd5deada77c0ed0436e642e547dfd
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-18 09:59:54 -0700

    Revert "try debug print in arimo.df.from_files._ArrowADF__prep__pandasDFTransform"
    This reverts commit 384a7cc05150f3e045fbf1f5dbd75052ac281089.

--- arimo/df/from_files.py
+++ arimo/df/from_files.py
@@ -257,8 +257,6 @@ class _ArrowADF__prep__pandasDFTransform:
                     with_std=True)
 
             # mean value for each feature in the training set
-            print([numPrepDetails['Mean']
-                   for numPrepDetails in self.numPrepDetails])
             self.numScaler.mean_ = \
                 numpy.array(
                     (numPrepDetails['Mean']
