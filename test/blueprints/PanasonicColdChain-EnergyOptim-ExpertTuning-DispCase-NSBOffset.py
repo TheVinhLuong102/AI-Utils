d65f9a38374ab406a089a55e96b028191039fa25
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-20 16:52:42 -0700

    remove old scripts

--- test/blueprints/PanasonicColdChain-EnergyOptim-ExpertTuning-DispCase-NSBOffset.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from __init__ import PANCC_REPO_DIR_PATH
-
-
-os.system(
-    'python {}/PanasonicColdChain/EnergyOptim/PENG-Apps/ExpertTuning/DispCase/NSBOffset/train.py --debug'
-        .format(PANCC_REPO_DIR_PATH))
