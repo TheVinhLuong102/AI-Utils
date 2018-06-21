ae00ffa05ac28b231ed484f277cd410c79f69267
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-21 16:54:54 -0700

    minor edits to test/_ISSUES/SparkSQL-read-multi-paths script

--- test/_ISSUES/SparkSQL-read-multi-paths.py
+++ test/_ISSUES/SparkSQL-read-multi-paths.py
@@ -12,6 +12,7 @@ arimo.backend.initSpark()
 
 
 # assertion failed: Conflicting directory structures detected. Suspicious paths: ...
-# If provided paths are partition directories, please set "basePath" in the options of the data source to specify the root directory of the table.
-#  If there are multiple root directories, please load them separately and then union them.
+# If provided paths are partition directories,
+# please set "basePath" in the options of the data source to specify the root directory of the table.
+# If there are multiple root directories, please load them separately and then union them.
 sdf = arimo.backend.spark.read.parquet(BIG_DATA_S3A_AUTH_PARQUET_PATH, BIG_DATA_S3A_AUTH_RAND_PARTITIONED_PARQUET_PATH)
