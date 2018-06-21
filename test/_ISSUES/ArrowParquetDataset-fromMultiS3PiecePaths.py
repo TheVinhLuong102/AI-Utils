313123f559b438def01d178a9ddc5c19347210f9
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-21 16:26:11 -0700

    add test/_ISSUES.ArrowParquetDataset-fromMultiS3PiecePaths script

---
+++ test/_ISSUES/ArrowParquetDataset-fromMultiS3PiecePaths.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from pyarrow.parquet import ParquetDataset
+from s3fs import S3FileSystem
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from data import _AWS_ACCESS_KEY_ID, _AWS_SECRET_ACCESS_KEY, SMALL_DATA_S3_PATH
+
+
+fs = S3FileSystem(key=_AWS_ACCESS_KEY_ID, secret=_AWS_ACCESS_KEY_ID)
+
+
+ds0 = ParquetDataset(
+    path_or_paths=SMALL_DATA_S3_PATH,
+    filesystem=fs,
+    schema=None, validate_schema=False, metadata=None,
+    split_row_groups=False)
+
+
+ds1 = ParquetDataset(
+    path_or_paths=list(piece.path for piece in ds0.pieces[:2]),
+    filesystem=fs,
+    schema=None, validate_schema=False, metadata=None,
+    split_row_groups=False)
