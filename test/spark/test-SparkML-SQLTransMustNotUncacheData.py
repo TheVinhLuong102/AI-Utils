9fec4fc0ef05d03ebb91054edd01334f897f0e93
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-21 14:30:03 -0700

    move test-SparkML-SQLTransMustNotUncacheData script

--- test/spark/test-SparkML-SQLTransMustNotUncacheData.py
+++ test/spark/test-SparkML-SQLTransMustNotUncacheData.py
@@ -7,10 +7,10 @@ from pyspark.ml.feature import SQLTransformer
 spark = pyspark.sql.SparkSession.builder.getOrCreate()
 
 
-df = spark.createDataFrame(pandas.DataFrame(dict(x=[-1, 0, 1])))
+sdf = spark.createDataFrame(pandas.DataFrame(dict(x=[-1, 0, 1])))
 
 # after below step, SparkUI Storage shows 1 cached RDD
-df.cache(); df.count()
+sdf.cache(); sdf.count()
 
 # after below step, cached RDD disappears from SparkUI Storage
-new_df = SQLTransformer(statement='SELECT * FROM __THIS__').transform(df)
+new_sdf = SQLTransformer(statement='SELECT * FROM __THIS__').transform(sdf)
