165a682e6a2477b562d253977d62e6e39087cc09
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-21 14:30:38 -0700

    refactor test-ADFs-split script

--- test/df/test-ADFs-split.py
+++ test/df/test-ADFs-split.py
@@ -9,6 +9,7 @@ if sys.version_info.major == 3:
     from functools import reduce
 
 from arimo.df.spark import SparkADF
+from arimo.df.from_files import ArrowADF
 
 
 N_IDS = 3
@@ -28,39 +29,44 @@ SPLIT_ADF_T_CHUNK_LEN = 3
 ORIG_T_CHUNK_LEN = 2 * SPLIT_ADF_T_CHUNK_LEN
 
 
+df = pandas.DataFrame(
+    data={
+        ID_COL_NAME:
+            reduce(
+                lambda x, y: x + y,
+                [N_TIME_STEPS_PER_ID * ['i{}'.format(i)]
+                 for i in ID_RANGE]),
+
+        TIME_COL_NAME:
+            [datetime.date(year=YEAR, month=m, day=d)
+             for m, d in itertools.product(ID_RANGE, TIME_ORD_RANGE)],
+
+        CONTENT_COL_NAME:
+            range(11, N_ROWS + 11)
+    }).sample(
+        n=N_ROWS,
+        frac=None,
+        replace=False,
+        weights=None,
+        random_state=None,
+        axis=None)
+
+
 adf = SparkADF.create(
-    data=pandas.DataFrame(
-        data={
-            ID_COL_NAME:
-                reduce(
-                    lambda x, y: x + y,
-                    [N_TIME_STEPS_PER_ID * ['i{}'.format(i)]
-                     for i in ID_RANGE]),
-
-            TIME_COL_NAME:
-                [datetime.date(year=YEAR, month=m, day=d)
-                 for m, d in itertools.product(ID_RANGE, TIME_ORD_RANGE)],
-
-            CONTENT_COL_NAME:
-                range(11, N_ROWS + 11)
-        }).sample(
-            n=N_ROWS,
-            frac=None,
-            replace=False,
-            weights=None,
-            random_state=None,
-            axis=None),
+    data=df,
     iCol=ID_COL_NAME,
     tCol=TIME_COL_NAME,
     tChunkLen=ORIG_T_CHUNK_LEN)
 
 
-adf_1, adf_2 = adf.split(.5, .5)
+adf_1, adf_2 = adf.split(.68, .32)
 
 adf_1.tChunkLen = adf_2.tChunkLen = SPLIT_ADF_T_CHUNK_LEN
 
 print('Split SparkADF #1:')
-adf_1.show(N_ROWS // 2)
+adf_1.show(N_ROWS)
 
-print('Split SparkADF #2')
-adf_2.show(N_ROWS // 2)
+print('Split SparkADF #2:')
+adf_2.show(N_ROWS)
+
+
