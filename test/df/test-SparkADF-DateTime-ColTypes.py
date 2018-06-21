34634b3ca90891eaadcc53a51b9824163b4a0376
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-06-21 10:59:57 -0700

    refactor test-SparkADF-DateTime-ColTypes script

--- test/df/test-SparkADF-DateTime-ColTypes.py
+++ test/df/test-SparkADF-DateTime-ColTypes.py
@@ -17,8 +17,11 @@ ts_adf_schema = ts_adf.schema
 
 
 for t_aux_col in ts_adf.tAuxCols:
-    _ts_adf_schema = ts_adf.rm(t_aux_col).schema
+    # try dropping & re-creating t_aux_col
+    _ts_adf_schema = ts_adf.drop(t_aux_col).schema
+
     if _ts_adf_schema != ts_adf_schema:
         assert [col for col in cols if _ts_adf_schema[col] != ts_adf_schema[col]] == [t_aux_col]
+
         assert (ts_adf_schema[t_aux_col].dataType.simpleString() == _INT_TYPE) \
            and (_ts_adf_schema[t_aux_col].dataType.simpleString() == _BIGINT_TYPE)
