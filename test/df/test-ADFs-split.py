5708517921193d8e0cf550b3608a9cd7eb288a0e
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-07-01 17:28:42 -0700

    update test-ADFs-split script

--- test/df/test-ADFs-split.py
+++ test/df/test-ADFs-split.py
@@ -2,12 +2,14 @@ from __future__ import print_function
 
 import datetime
 import itertools
+import os
 import pandas
 
 import sys
 if sys.version_info.major == 3:
     from functools import reduce
 
+import arimo.backend
 from arimo.df.spark import SparkADF
 from arimo.df.from_files import ArrowADF
 
@@ -27,6 +29,13 @@ CONTENT_COL_NAME = 'x'
 
 SPLIT_ADF_T_CHUNK_LEN = 3
 ORIG_T_CHUNK_LEN = 2 * SPLIT_ADF_T_CHUNK_LEN
+
+
+PARQUET_LOCAL_PATH = \
+    os.path.join(
+        os.path.dirname(os.path.dirname(arimo.backend.__file__)),
+        'resources',
+        'skewed-data.parquet')
 
 
 df = pandas.DataFrame(
@@ -52,21 +61,30 @@ df = pandas.DataFrame(
         axis=None)
 
 
-adf = SparkADF.create(
-    data=df,
-    iCol=ID_COL_NAME,
-    tCol=TIME_COL_NAME,
-    tChunkLen=ORIG_T_CHUNK_LEN)
+spark_adf = \
+    SparkADF.create(
+        data=df,
+        iCol=ID_COL_NAME,
+        tCol=TIME_COL_NAME,
+        tChunkLen=ORIG_T_CHUNK_LEN)
 
 
-adf_1, adf_2 = adf.split(.68, .32)
+spark_adf_1, spark_adf_2 = spark_adf.split(.68, .32)
 
-adf_1.tChunkLen = adf_2.tChunkLen = SPLIT_ADF_T_CHUNK_LEN
+spark_adf_1.tChunkLen = spark_adf_2.tChunkLen = SPLIT_ADF_T_CHUNK_LEN
 
 print('Split SparkADF #1:')
-adf_1.show(N_ROWS)
+spark_adf_1.show(N_ROWS)
 
 print('Split SparkADF #2:')
-adf_2.show(N_ROWS)
+spark_adf_2.show(N_ROWS)
 
 
+arrow_adf = ArrowADF(path=PARQUET_LOCAL_PATH)
+print(arrow_adf)
+
+
+arrow_adf_1, arrow_adf_2 = arrow_adf.split(.68, .32)
+
+print('Split ArrowADF #1: {}'.format(arrow_adf_1))
+print('Split ArrowADF #2: {}'.format(arrow_adf_2))
