9a3f5e15cbd45c7c3d3a91e5b73ca088a4049b80
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-08-19 00:22:24 -0700

    clean up legacy code

--- arimo/df/dask.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from dask.dataframe import DataFrame
-
-from . import _ADFABC
-
-
-class DaskADF(_ADFABC):
-    pass
--- arimo/df/dask_from_files.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .dask import DaskADF
-
-
-class ArrowDaskADF(DaskADF):
-    pass
