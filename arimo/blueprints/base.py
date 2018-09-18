a7a614f48c35e305ea263edb959d0a0e6926d330
Vinh Luong <MBALearnsToCode@UChicago.edu>
2018-09-18 09:08:53 -0700

    minor fix to arimo.blueprints.base._PPPBlueprintABC.ewma_daily_err_mults(...)

--- arimo/blueprints/base.py
+++ arimo/blueprints/base.py
@@ -3255,32 +3255,30 @@ class _PPPBlueprintABC(_BlueprintABC):
                     lambda df:
                         df.where(
                             cond=pandas.isnull(df),
-                            other=df.ewm(
-                                    com=None,
-                                    span=None,
-                                    halflife=None,
-                                    alpha=_alpha,
-                                    min_periods=0,
-                                    adjust=False,
-                                        # ref: http://pandas.pydata.org/pandas-docs/stable/computation.html#exponentially-weighted-windows
-                                    ignore_na=False,
-                                        # When ignore_na is False (default), weights are based on absolute positions.
-                                        # For example, the weights of x and y used in calculating the final weighted average of
-                                        # [x, None, y] are (1-alpha)**2 and 1 (if adjust is True),
-                                        # and (1-alpha)**2 and alpha (if adjust is False).
-                                        # When ignore_na is True (reproducing pre-0.15.0 behavior),
-                                        # weights are based on relative positions.
-                                        # For example, the weights of x and y used in calculating the final weighted average of
-                                        # [x, None, y] are 1-alpha and 1 (if adjust is True),
-                                        # and 1-alpha and alpha (if adjust is False).
-                                    axis='index')
-                                .mean()),
+                            other=df.ewm(com=None,
+                                         span=None,
+                                         halflife=None,
+                                         alpha=_alpha,
+                                         min_periods=0,
+                                         adjust=False,
+                                            # ref: http://pandas.pydata.org/pandas-docs/stable/computation.html#exponentially-weighted-windows
+                                         ignore_na=False,
+                                            # When ignore_na is False (default), weights are based on absolute positions.
+                                            # For example, the weights of x and y used in calculating the final weighted average of
+                                            # [x, None, y] are (1-alpha)**2 and 1 (if adjust is True),
+                                            # and (1-alpha)**2 and alpha (if adjust is False).
+                                            # When ignore_na is True (reproducing pre-0.15.0 behavior),
+                                            # weights are based on relative positions.
+                                            # For example, the weights of x and y used in calculating the final weighted average of
+                                            # [x, None, y] are 1-alpha and 1 (if adjust is True),
+                                            # and 1-alpha and alpha (if adjust is False).
+                                         axis='index').mean(),
                             inplace=False,
                             axis=None,
                             level=None,
                             errors='raise',
                             try_cast=False,
-                            raise_on_error=None)
+                            raise_on_error=None))
 
         return daily_err_mults_df
 
