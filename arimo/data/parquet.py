dba47a551f6cb576e5cdc502b718607a65afe099
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-07-25 10:13:11 -0700

    minor fix to arimo.data.parquet

--- arimo/data/parquet.py
+++ arimo/data/parquet.py
@@ -791,7 +791,7 @@ class S3ParquetDataFeeder(AbstractS3ParquetDataHandler):
                 _cache.s3Bucket = _parsedURL.netloc
                 _cache.pathS3Key = _parsedURL.path[1:]
 
-                _cache.tmpDirS3Key = '/tmp'
+                _cache.tmpDirS3Key = 'tmp'
 
                 _cache.tmpDirPath = \
                     os.path.join(
