8a9bb2d219d1a710ef7cff638a67b67873dd09f9
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-07-31 10:31:58 -0700

    remove comments in arimo.blueprints.cs.regr.DLBlueprint(...)

--- arimo/blueprints/cs/regr.py
+++ arimo/blueprints/cs/regr.py
@@ -248,136 +248,37 @@ class DLBlueprint(RegrEvalMixIn, AbstractDLCrossSectSupervisedBlueprint):
             model.history = \
                 model.fit_generator(
                     generator=train_gen(),
-                        # a generator.
-                        # The output of the generator must be either a tuple(inputs, targets)
-                        # or a tuple(inputs, targets, sample_weights).
-                        # Therefore, all arrays in this tuple must have the same length (equal to the size of this batch).
-                        # Different batches may have different sizes.
-                        # For example, the last batch of the epoch is commonly smaller than the others,
-                        # if the size of the dataset is not divisible by the batch size.
-                        # The generator is expected to loop over its data indefinitely.
-                        # An epoch finishes when steps_per_epoch batches have been seen by the model.
-
                     steps_per_epoch=self.params.model.train._n_train_batches_per_epoch,
-                        # Total number of steps (batches of samples) to yield from generator
-                        # before declaring one epoch finished and starting the next epoch.
-                        # It should typically be equal to the number of samples of your dataset divided by the batch size.
-
                     epochs=self.params.model.train._n_epochs,
-                        # Integer, total number of iterations on the data.
-                        # Note that in conjunction with initial_epoch, the parameter epochs is to be understood as "final epoch".
-                        # The model is not trained for n steps given by epochs, but until the epoch epochs is reached.
-
                     verbose=2,
-                        # Integer. 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-
                     callbacks=[
                         arimo.backend.keras.callbacks.TerminateOnNaN(),
-                            # Callback that terminates training when a NaN loss is encountered.
-
                         arimo.backend.keras.callbacks.ReduceLROnPlateau(
-                            # Reduce learning rate when a metric has stopped improving.
-                            # Models often benefit from reducing the learning rate by a factor of 2-10 once learning stagnates.
-                            # This callback monitors a quantity and if no improvement is seen for a 'patience' number of epochs,
-                            # the learning rate is reduced.
-
                             monitor=self.params.model.train.val_metric.name,
-                                # quantity to be monitored.
-
                             factor=self.params.model.train.reduce_lr_on_plateau.factor,
-                                # factor by which the learning rate will be reduced. new_lr = lr * factor
-
                             patience=self.params.model.train.reduce_lr_on_plateau.patience_n_epochs,
-                                # number of epochs with no improvement after which learning rate will be reduced.
-
                             verbose=int(verbose > 0),
-                                # int. 0: quiet, 1: update messages.
-
                             mode=self.params.model.train.val_metric.mode,
-                                # one of {auto, min, max}.
-                                # In min mode, lr will be reduced when the quantity monitored has stopped decreasing;
-                                # In max mode it will be reduced when the quantity monitored has stopped increasing;
-                                # In auto mode, the direction is automatically inferred from the name of the monitored quantity.
-
                             min_delta=self.params.model.train.val_metric.significance,
-                                # threshold for measuring the new optimum, to only focus on significant changes.
-
                             cooldown=0,
-                                # number of epochs to wait before resuming normal operation after lr has been reduced.
-
-                            min_lr=0
-                                # lower bound on the learning rate.
-                            ),
-
+                            min_lr=0),
                         arimo.backend.keras.callbacks.EarlyStopping(
-                            # Stop training when a monitored quantity has stopped improving.
-
                             monitor=self.params.model.train.val_metric.name,
-                                # quantity to be monitored.
-
                             min_delta=self.params.model.train.val_metric.significance,
-                                # minimum change in the monitored quantity to qualify as an improvement,
-                                # i.e. an absolute change of less than min_delta, will count as no improvement.
-
                             patience=max(self.params.model.train.early_stop.patience_min_n_epochs,
                                          int(math.ceil(self.params.model.train.early_stop.patience_proportion_total_n_epochs *
                                                       self.params.model.train._n_epochs))),
-                                # number of epochs with no improvement after which training will be stopped.
-
                             verbose=int(verbose),
-                                # verbosity mode.
-
                             mode=self.params.model.train.val_metric.mode,
-                                # one of {auto, min, max}.
-                                # In min mode, training will stop when the quantity monitored has stopped decreasing;
-                                # In max mode it will stop when the quantity monitored has stopped increasing;
-                                # In auto mode, the direction is automatically inferred from the name of the monitored quantity.
-
-                            baseline=None
-                                # Baseline value for the monitored quantity to reach.
-                                # Training will stop if the model doesn't show improvement over the baseline.
-                            )],
-
+                            baseline=None)],
                     validation_data=val_gen(),
-                        # this can be either:
-                        # - a generator for the validation data;
-                        # - a tuple(inputs, targets); or
-                        # - a tuple(inputs, targets, sample_weights).
-
                     validation_steps=self.params.model.train._n_val_batches_per_epoch,
-                        # Only relevant if validation_data is a generator.
-                        # Total number of steps (batches of samples) to yield from  validation_data generator
-                        # before stopping at the end of every epoch.
-                        # It should typically be equal to the number of samples of your validation dataset divided by the batch size.
-
                     class_weight={},
-                        # Optional dictionary mapping class indices (integers) to a weight (float) value,
-                        # used for weighting the loss function (during training only).
-                        # This can be useful to tell the model to "pay more attention" to samples from an under-represented class.
-
                     max_queue_size=__gen_queue_size__,
-                        # Integer. Maximum size for the generator queue.
-                        # If unspecified, max_queue_size will default to 10
-
                     workers=__n_workers__,
-                        # Integer. Maximum number of processes to spin up when using process-based threading.
-                        # If unspecified, workers will default to 1. If 0, will execute the generator on the main thread.
-
                     use_multiprocessing=__multiproc__,
-                        # Boolean. If True, use process-based threading.
-                        # If unspecified, use_multiprocessing will default to False.
-                        # Note that because this implementation relies on multiprocessing,
-                        # you should not pass non-picklable arguments to the generator
-                        # as they can't be passed easily to children processes.
-
                     shuffle=False,
-                        # Boolean (whether to shuffle the order of the batches at the beginning of each epoch
-                        # Only used with instances of Sequence (keras.utils.Sequence).
-                        # Has no effect when steps_per_epoch is not None.
-
-                    initial_epoch=0
-                        # epoch at which to start training (useful for resuming a previous training run)
-                    ) \
+                    initial_epoch=0) \
                 .history
 
         model.save()
