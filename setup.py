f71b4bf578fafd15c315d6a2f10e38d8aaf641d2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-03-07 20:38:50 -0800

    update setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from ruamel import yaml
-from setuptools import find_namespace_packages, find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 
 _PACKAGE_NAMESPACE_NAME = 'arimo'
@@ -8,7 +8,6 @@ _PACKAGE_NAMESPACE_NAME = 'arimo'
 _METADATA_FILE_NAME = 'metadata.yml'
 
 _SETUP_REQUIREMENTS_FILE_NAME = 'requirements-setup.txt'
-
 _INSTALL_REQUIREMENTS_FILE_NAME = 'requirements.txt'
 
 
@@ -20,25 +19,17 @@ _metadata = \
                 _METADATA_FILE_NAME)))
 
 
+def parse_requirements(requirements_file_name):
+    return [s for s in
+                {i.strip()
+                 for i in open(requirements_file_name).readlines()}
+            if not s.startswith('#')]
+
+
 setup(
     name=_metadata['PACKAGE'],
-    author=_metadata['AUTHOR'],
-    author_email=_metadata['AUTHOR_EMAIL'],
-    url=_metadata['URL'],
     version=_metadata['VERSION'],
-    description=_metadata['DESCRIPTION'],
-    long_description=_metadata['DESCRIPTION'],
-    keywords=_metadata['DESCRIPTION'],
     namespace_packages=[_PACKAGE_NAMESPACE_NAME],
     packages=find_namespace_packages(include=[f'{_PACKAGE_NAMESPACE_NAME}.*']),
-    include_package_data=True,
-    setup_requires=
-        [s for s in
-            {i.strip()
-             for i in open(_SETUP_REQUIREMENTS_FILE_NAME).readlines()}
-         if not s.startswith('#')],
-    install_requires=
-        [s for s in
-            {i.strip()
-             for i in open(_INSTALL_REQUIREMENTS_FILE_NAME).readlines()}
-         if not s.startswith('#')])
+    setup_requires=parse_requirements(_SETUP_REQUIREMENTS_FILE_NAME),
+    install_requires=parse_requirements(_INSTALL_REQUIREMENTS_FILE_NAME))
