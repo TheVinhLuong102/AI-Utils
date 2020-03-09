a52d8e643293bdd32f1571c9bd6efe9bd335b6cd
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-03-09 13:40:17 -0700

    optimize Spark worker size c5n.9xlarge

--- arimo/util/data_backend/__init__.py
+++ arimo/util/data_backend/__init__.py
@@ -613,7 +613,7 @@ def initSpark(
         yarnConfDir=None,
         yarnUpdateJARs=False,
         dataIO={'avro', 'pg', 'redshift', 'sftp'},
-        executor_aws_ec2_instance_type='c5n.metal'):
+        executor_aws_ec2_instance_type='c5n.9xlarge'):
     """
     Launch new ``SparkSession`` or connect to existing one, and binding it to ``arimo.data_backend.spark``
 
