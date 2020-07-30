b4b35c91b67698ab33f7063ac385cc142b073de2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-07-29 22:32:09 -0700

    fix AWS-Java-SDK version

--- arimo/util/data_backend/__init__.py
+++ arimo/util/data_backend/__init__.py
@@ -236,8 +236,8 @@ _SPARK_REPOS = \
 
 _SPARK_PKGS = {
     # AWS-related dependencies
-    'com.amazonaws:aws-java-sdk:1.11.828',
     'org.apache.hadoop:hadoop-aws:2.7.7',   # consistent with Hadoop 2.7
+    'com.amazonaws:aws-java-sdk:1.7.4',   # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-aws/2.7.7
 
     # Spark DL Pipelines & TensorFrames
     # 'databricks:spark-deep-learning:1.5.0-spark2.4-s_2.11',
