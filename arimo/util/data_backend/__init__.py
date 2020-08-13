76b026870e63b3dfd07665496872ef713d60e8f4
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-08-13 00:57:44 -0700

    fix python3 on Spark driver & workers

--- arimo/util/data_backend/__init__.py
+++ arimo/util/data_backend/__init__.py
@@ -647,9 +647,9 @@ def initSpark(
     logger.addHandler(STDOUT_HANDLER)
 
     # driver Python executable path
-    os.environ['PYSPARK_DRIVER_PYTHON'] = 'python3.8'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = 'python3'
     # worker Python executable path
-    os.environ['PYSPARK_PYTHON'] = '/opt/miniconda3.8/bin/python3.8'   # TODO: make generic 'python3.7'
+    os.environ['PYSPARK_PYTHON'] = '/opt/miniconda3.8/bin/python3'   # TODO: make generic 'python3'
 
     # set relevant environment variables for Java, Spark, Hadoop & YARN
     if javaHome:
