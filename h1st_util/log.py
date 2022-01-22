0443df4848f0014af154e1615dbf6e59cdb84135
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-01-21 22:56:19 -0800

    refactor h1st_util.log

--- h1st_util/log.py
+++ h1st_util/log.py
@@ -1,54 +1,44 @@
+"""Logging-related utilities."""
+
+
 import logging
 import sys
 
 
-# BUG FIX: https://github.com/tensorflow/tensorflow/issues/26691
-try:
-    # Capirca uses Google's abseil-py library, which uses a Google-specific
-    # wrapper for logging. That wrapper will write a warning to sys.stderr if
-    # the Google command-line flags library has not been initialized.
-    # https://github.com/abseil/abseil-py/blob/pypi-v0.7.1/absl/logging/__init__.py#L819-L825
-    # This is not right behavior for Python code that is invoked outside of a
-    # Google-authored main program. Use knowledge of abseil-py to disable that
-    # warning; ignore and continue if something goes wrong.
-    import absl.logging
-
-    # https://github.com/abseil/abseil-py/issues/99
-    logging.root.removeHandler(absl.logging._absl_handler)
-    # https://github.com/abseil/abseil-py/issues/102
-    absl.logging._warn_preinit_stderr = False
-except Exception:
-    pass
+__all__ = 'STDOUT_HANDLER', 'enable_live_print'
 
 
 # handler for logging to StdOut
-STDOUT_HANDLER = \
-    logging.StreamHandler(sys.stdout)
+STDOUT_HANDLER = logging.StreamHandler(stream=sys.stdout)
 
 STDOUT_HANDLER.setFormatter(
-    logging.Formatter(
+    fmt=logging.Formatter(
         fmt='%(asctime)s   %(levelname)s   %(name)s:   %(message)s\n',
-        datefmt='%Y-%m-%d %H:%M'))
+        datefmt='%Y-%m-%d %H:%M',
+        style='%',
+        validate=True))
 
 
 # utility class to flush logging stream upon each write
-# http://stackoverflow.com/questions/29772158/make-ipython-notebook-print-in-real-time
+# stackoverflow.com/questions/29772158/make-ipython-notebook-print-in-real-time
 class _FlushFile:
-    def __init__(self, f):
-        self.f = f
+    def __init__(self, f):   # pylint: disable=invalid-name
+        self.f = f   # pylint: disable=invalid-name
 
     def __getattr__(self, item):
         return self.f.__getattribute__(item)
 
     def flush(self):
+        """Flush."""
         self.f.flush()
 
-    def write(self, x):
+    def write(self, x):   # pylint: disable=invalid-name
+        """Write."""
         self.f.write(x)
         self.flush()
 
 
-# enable live printing
 def enable_live_print():
+    """Enable live printing."""
     sys.stdout = _FlushFile(sys.stdout)
     print('Live Printing Enabled')
