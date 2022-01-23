814a2e7af3f80371e11c9d1c74ba514d9e9c23d7
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-01-22 17:47:34 -0800

    refactor h1st_util.log

--- h1st_util/log.py
+++ h1st_util/log.py
@@ -3,6 +3,7 @@
 
 from logging import Formatter, StreamHandler
 import sys
+from typing import Any
 
 
 __all__ = 'STDOUT_HANDLER', 'enable_live_print'
@@ -12,27 +13,26 @@ __all__ = 'STDOUT_HANDLER', 'enable_live_print'
 STDOUT_HANDLER: StreamHandler = StreamHandler(stream=sys.stdout)
 
 STDOUT_HANDLER.setFormatter(
-    fmt=Formatter(
-        fmt='%(asctime)s   %(levelname)s   %(name)s:   %(message)s\n',
-        datefmt='%Y-%m-%d %H:%M',
-        style='%',
-        validate=True))
+    fmt=Formatter(fmt='%(asctime)s   %(levelname)s   %(name)s:   %(message)s\n',   # noqa: E501
+                  datefmt='%Y-%m-%d %H:%M',
+                  style='%',
+                  validate=True))
 
 
 # utility class to flush logging stream upon each write
 # stackoverflow.com/questions/29772158/make-ipython-notebook-print-in-real-time
 class _FlushFile:
-    def __init__(self, f):   # pylint: disable=invalid-name
+    def __init__(self, f, /):   # pylint: disable=invalid-name
         self.f = f   # pylint: disable=invalid-name
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str, /):
         return self.f.__getattribute__(item)
 
     def flush(self):
         """Flush."""
         self.f.flush()
 
-    def write(self, x):   # pylint: disable=invalid-name
+    def write(self, x: Any, /):   # pylint: disable=invalid-name
         """Write."""
         self.f.write(x)
         self.flush()
